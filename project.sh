d6b2a443f71351dba332e8cdb69966d9ae132948
Alexandru Burbea <alexandru.burbea01@e-uvt.ro>
2024-12-28 18:07:23 +0200

    New Update

--- project.sh
+++ project.sh
@@ -6,11 +6,12 @@ echo "3)    Deleting files periodically (the ones older than 60 days, every mond
 echo "4)    Configuration options: deleting files, renaming files, editing files, adding 4 more options (to be updated), adding options for '--help', '--usage', 'debugging mode'"
 echo "-----------------------------------------------------------"
 
-select option in "Find old files" "Moving files locally" "Moving files in cloud" "Deleting certain files" "Configuration options"; do
+select option in "Find old files" "Moving files locally" "Moving files in cloud" "Deleting certain files" "Delete selected file" "Rename selected file" "Edit files from a selected folder" "Configuration options"; do
     case $option in
         "Find old files")
             echo "1) To be updated"
             ;;
+
         "Moving files locally")
             read -rp "Source path: " src;
             read -rp "Destination path: " destination;
@@ -21,19 +22,45 @@ select option in "Find old files" "Moving files locally" "Moving files in cloud"
 
             mv $src $destination
             ;;
+
         "Moving files in cloud")
             read -rp "Source path: " src2;
             if [-z "$src2"]; then
                 echo "Error [Path doesn't exist]" 2> error.txt
             fi
 
+            git add $src2
+            git commit -m "File moved in cloud"
+            git push -u origin main
             ;;
+
         "Deleting certain files")
-            echo "3) To be updated"
+            read -rp "Selected path: " path
+            find $path -type f -mtime +60 -exec rm -f {} \;
             ;;
+
+        "Delete selected file")
+            echo "To be updated"
+            ;;
+
+        "Rename selected file")
+            read -rp "Selected path: " path2
+            for file in "$path2"/*; do
+                mv $file "$file".old
+            done 
+            ;;
+
+        "Edit files from a selected folder")
+            read -rp "Selected path: " path2
+            for file in "$path2"/*; do
+                echo "####Deprecated####" > $file
+            done 
+            ;;
+
         "Configuration options")
             echo "4) To be updated"
             ;;
+
         *)
             echo "Invalid option"
             break;
